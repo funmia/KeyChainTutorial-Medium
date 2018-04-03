f6e53b497584dbdd215e9d18ddb84e97b0185407
funmia <funmiadewodu@yahoo.com>
2018-04-03 23:44:28 +0100

    Adds functions to store and retrieve data from Keychain

--- KeyChainTutorialMedium/ViewController.swift
+++ KeyChainTutorialMedium/ViewController.swift
@@ -7,19 +7,36 @@
 //
 
 import UIKit
+import SwiftKeychainWrapper
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func savePasswordButtonTapped(_ sender: UIButton) {
+        
+        if let password = passwordTextField.text {
+            
+            let saveSuccessful: Bool = KeychainWrapper.standard.set(password, forKey: "userPassword")
+            print("Save was successful: \(saveSuccessful)")
+            self.view.endEditing(true)
+        }
     }
+    
 
-
+    @IBAction func retrievePasswordButtonTapped(_ sender: UIButton) {
+        
+        let retrievedPassword: String? = KeychainWrapper.standard.string(forKey: "userPassword")
+        
+        if let retrievedPassword = retrievedPassword {
+            print("Retrieved password is: \(retrievedPassword)")
+        }
+    }
+    
 }
 
